a96bbd3fadd36baa78d693fc9a9d2bda86867568
Akshay <aksrajvanshi@gmail.com>
2020-01-03 18:13:23 -0500

    Updates

---
+++ precache-manifest.5b1088e34528d6cce1ae8d4aaa9b8c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb27d9b81e30ea9935c7360a3a7e42b4",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4e69bf69bacf143e0179",
+    "url": "/my-portfolio/static/css/main.866a55ab.chunk.css"
+  },
+  {
+    "revision": "78656efedf6b403950fe",
+    "url": "/my-portfolio/static/js/2.e5e44a61.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/my-portfolio/static/js/2.e5e44a61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e69bf69bacf143e0179",
+    "url": "/my-portfolio/static/js/main.bca16b7b.chunk.js"
+  },
+  {
+    "revision": "67df657eb7832c33269d",
+    "url": "/my-portfolio/static/js/runtime-main.7873a797.js"
+  }
+]);
