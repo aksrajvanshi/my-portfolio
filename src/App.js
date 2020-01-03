3bc8ce5d2b39c146dd150747f38c6da9a0dd6f92
Akshay <aksrajvanshi@gmail.com>
2020-01-03 18:11:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import About from "./components/About";
+import Introduction from "./components/Introduction";
+import Sidebar from "./components/Sidebar";
+import Timeline from "./components/Timeline";
+import WhatIDo from "./components/WhatIDo";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div id="colorlib-page">
+                <div id="container-wrap">
+                    <Sidebar></Sidebar>
+                    <div id="colorlib-main">
+                        <Introduction></Introduction>
+                        <About></About>
+                        <WhatIDo></WhatIDo>
+                        <Timeline></Timeline>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <section className="colorlib-about" data-section="about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                                    <div className="col-md-12">
+                                        <div className="about-desc">
+                                            <span className="heading-meta">About Us</span>
+                                            <h2 className="colorlib-heading">Who Am I?</h2>
+                                            <p>I am a computer science grad student from IU Bloomington. I am a curious and motivated engineer who loves learning and exploring new technologies. I'm currently in my 2nd semester
+                                            of my masters. I took courses such Cloud Computing, Algorithms and Software Engineering in my 1st semester. </p>
+                                            <p></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
