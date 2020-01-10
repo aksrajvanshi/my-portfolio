758587d603748159a58c2b4b9266d06778ead3d4
Akshay <aksrajvanshi@gmail.com>
2020-01-10 00:37:09 -0500

    Changed content for the timeline.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -17,12 +17,25 @@ export default class Timeline extends React.Component {
                                 <div className="timeline-centered">
                                     <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
                                         <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-1">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>Associate Instructor at IU <span> Jan 2020- Present</span></h2>
+                                                <p>As a longing dream for my masters, I get to conduct labs and teach Java to about 20 undergrad students and spread the knowledge
+                                                I gained from my experience. I will be assisting the instructor with grading for the <b>CSCI C212-Introduction to Software Systems</b> course.</p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                                        <div className="timeline-entry-inner">
                                             <div className="timeline-icon color-3">
                                                 <i className="icon-pen2" />
                                             </div>
                                             <div className="timeline-label">
-                                                <h2>Graduate CS Student at Indiana University Bloomington <span> 2019-present</span></h2>
-                                                <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                                                <h2>Graduate CS Student at Indiana University Bloomington <span> 2019- Present</span></h2>
+                                                <p>After a long stint as a professional developer, I decided to head back to study and get my Masters at IU. Right now, I am studying Computer Science
+                                                and enjoying the learning experience I get from professors and my peers. In my first semester, I took up courses such as <b>cloud computing, software engineering and applied algorithms</b>.</p>
                                             </div>
                                         </div>
                                     </article>
@@ -33,7 +46,9 @@ export default class Timeline extends React.Component {
                                             </div>
                                             <div className="timeline-label">
                                                 <h2>Java Developer at SBI-BITS<span> April 2017- July 2019</span></h2>
-                                                <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
+                                                <p>I joined a financial services firm, SBI-BITS and further extended my knowledge in Java. I got the chance to lead a small
+                                                team of 2 people where we shipped an overhauled system of the existing force settlement batch system for internal operations of the company. Moreover,
+                                                    I implemented fault-tolerant behavior for the backend of order processing servers. </p>
                                             </div>
                                         </div>
                                     </article>
@@ -44,7 +59,9 @@ export default class Timeline extends React.Component {
                                             </div>
                                             <div className="timeline-label">
                                                 <h2>Web Engineer at Hikari Tsushin <span> Oct 2014- Feb 2017</span></h2>
-                                                <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p>
+                                                <p>After finishing my undergrad, I got to work in Hikari Tsushin and experience an international environment and work culture. I worked on a variety
+                                                of projects ranging from Servelets, EJB and a telemarketing system to streamline the existing process of how business employees interacted
+                                                with their customers.</p>
                                             </div>
                                         </div>
                                     </article>
@@ -55,13 +72,8 @@ export default class Timeline extends React.Component {
                                             </div>
                                             <div className="timeline-label">
                                                 <h2>Undergrad from IIIT-Delhi  <span> August 2010- May 2014</span></h2>
-                                                <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p>
-                                            </div>
-                                        </div>
-                                    </article>
-                                    <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
-                                        <div className="timeline-entry-inner">
-                                            <div className="timeline-icon color-none">
+                                                <p>I studied Computer Science and Engineering from IIIT-Delhi and pursued my Bachelors in Technology degree. I took courses such as DSA, Computer Networks and Advanced Programming. I developed
+                                                interest in Java and its related frameworks and in the end, was ready to dive into the professional field to apply whatever I'd learned.</p>
                                             </div>
                                         </div>
                                     </article>
